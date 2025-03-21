6a1fc639f31214987c49a78c9e5854fa9d8e0dfe
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-21 20:40:17 +0330

    format code by adding blank lines for improved readability

--- src/main/java/tutorial/tutorial/TutorialApplication.java
+++ src/main/java/tutorial/tutorial/TutorialApplication.java
@@ -16,5 +16,7 @@ public class TutorialApplication {
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
+
+
 }
 
