cc50b9f5f69a4628f28fe404856788e0c242d2c5
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-22 12:02:20 +0330

    remove hello endpoint from TutorialApplication

--- src/main/java/tutorial/tutorial/TutorialApplication.java
+++ src/main/java/tutorial/tutorial/TutorialApplication.java
@@ -12,12 +12,9 @@ public class TutorialApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TutorialApplication.class, args);
 	}
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
-
 	
+
+
 
 
 }
